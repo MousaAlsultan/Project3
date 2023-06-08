6b09751a056c3bf300d00601a0e7f307a1c74cf6
Mousa Alsultan <77552754+MousaAlsultan@users.noreply.github.com>
2023-06-08 12:27:35 +0300

    Update apiRequests.js

--- Project3/apiRequests.js
+++ Project3/apiRequests.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 
 async function fetchMovieData(title) {
   try {
-    const response = await fetch(`http://www.omdbapi.com/?t=${title}&apikey=YOUR_API_KEY`);
+    const response = await fetch(`http://www.omdbapi.com/?t=${title}&apikey=a6c9985`);
     const data = await response.json();
     return data;
   } catch (error) {
